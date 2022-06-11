df822ac5b6027c29533369aeabf60dc58c33d83d
ITPini <marc6571us@gmail.com>
2022-06-12 00:14:58 +0200

    Created full project

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
-const express = require('express');
-const createError = require('http-errors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var express = require('express');
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 // Routes
-const indexRouter = require('./routes/index');
-const catalogRouter = require('./routes/catalog');
-const usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var catalogRouter = require('./routes/catalog');
+var usersRouter = require('./routes/users');
 
 // Application routes
-const app = express();
+var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Import mongoose
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb+srv://pini:fns83ydg@cluster0.o32ao.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true }, {useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 app.use('/catalog', catalogRouter);
 app.use('/users', usersRouter);
-
+//app.use('/users/cool', coolRouter);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
-  });
+  next(createError(404));
+});
   
   // Error handler
-  app.use(function(err, req, res, next) {
-    // Set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   
-    // Render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(3000, () => {
--- models/author.js
+++ models/author.js
@@ -1,26 +1,53 @@
-const mongoose = require("mongoose");
+var mongoose = require('mongoose');
+const { DateTime } = require("luxon");  //for date handling
 
-const authorSchema = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    date_of_birth: Date,
-    date_of_death: Date
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+    first_name: {type: String, required: true, maxLength: 100},
+    family_name: {type: String, required: true, maxLength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date}
 });
 
-// Generate full name of first_name and last_name
-authorSchema.virtual("full_name").get(function() {
-    return this.first_name + " " + this.last_name;
-});
-
-// Generate url of identity
-authorSchema.virtual("url").get(function() {
-    return "/catalog/author/" + this._id;
+// Generate full name of first_name and family_name
+AuthorSchema.virtual('name').get(function() {
+    var fullname = '';
+    if (this.first_name && this.family_name) {
+        fullname = this.first_name + ' ' + this.family_name;
+    }
+    // In case of error
+    if (!this.first_name && !this.family_name) {
+        fullname = '';
+    }
+    return fullname;
 });
 
 // Generate life span of date of birth and death
-authorSchema.virtual("life_span").get(function() {
-    return this.date_of_birth + " - " + this.date_of_death;
+AuthorSchema.virtual('lifespan').get(function() {
+    var lifetime_string = '';
+    if (this.date_of_birth) {
+        lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+    }
+    lifetime_string += ' - ';
+    if (this.date_of_death) {
+        lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    }
+    return lifetime_string;
+});
+
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function() {
+    return DateTime.fromJSDate(this.date_of_birth).toISODate(); //format 'YYYY-MM-DD'
+});
+  
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function() {
+    return DateTime.fromJSDate(this.date_of_death).toISODate(); //format 'YYYY-MM-DD'
+});
+
+// Generate url of identity
+AuthorSchema.virtual('url').get(function() {
+    return "/catalog/author/" + this._id;
 });
 
 // Export module
-module.exports = mongoose.model("Author", authorSchema);
+module.exports = mongoose.model('Author', AuthorSchema);
